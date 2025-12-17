85f933df58208df95aff1621080cbc19ed7de318
EraiicPhu <eraiicphu@gmail.com>
2025-12-17 14:23:55 +0100

    Created Entity for DiningTable

--- src/main/java/org/example/DiningTable.java
+++ src/main/java/org/example/DiningTable.java
@@ -3,16 +3,43 @@ package org.example;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Dining tables")
 public class DiningTable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String tableNumber;
+
+    private int seatCapacity;
 
     public Long getId() {
         return id;
+    }
+
+    public String getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(String tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public int getSeatCapacity() {
+        return seatCapacity;
+    }
+
+    public void setSeatCapacity(int seatCapacity) {
+        this.seatCapacity = seatCapacity;
+    }
+
+
+    @Override
+    public String toString() {
+        return "DiningTable{" +
+            "id=" + id +
+            ", tableNumber='" + tableNumber + '\'' +
+            ", seatCapacity=" + seatCapacity +
+            '}';
     }
 }
