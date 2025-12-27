3dbf1a756e16bd3fd1276e2e9e89e3a2665d12c7
Kathrin Trinh <trinhkathrin@hotmail.com>
2025-12-27 18:07:44 +0100

    Update DiningTableRepo.java

--- src/main/java/org/example/repositories/DiningTableRepo.java
+++ src/main/java/org/example/repositories/DiningTableRepo.java
@@ -1,17 +1,4 @@
 package org.example.repositories;
 
-import org.example.entities.DiningTable;
-import java.util.List;
-
-public interface DiningTableRepo {
-
-    void save(DiningTable table);
-
-    DiningTable findById(Long id);
-
-    List<DiningTable> findAll();
-
-    void update(DiningTable table);
-
-    void delete(DiningTable table);
+public class DiningTableRepo {
 }
