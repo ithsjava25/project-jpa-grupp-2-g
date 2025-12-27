095e08ef48d710068853693e705be723b9b682f8
Kathrin Trinh <trinhkathrin@hotmail.com>
2025-12-27 18:07:03 +0100

    Update CustomerRepo.java

--- src/main/java/org/example/repositories/CustomerRepo.java
+++ src/main/java/org/example/repositories/CustomerRepo.java
@@ -1,17 +1,4 @@
 package org.example.repositories;
 
-import org.example.entities.Customer;
-import java.util.List;
-
-public interface CustomerRepo {
-
-    void save(Customer customer);
-
-    Customer findById(Long id);
-
-    List<Customer> findAll();
-
-    void update(Customer customer);
-
-    void delete(Customer customer);
+public class CustomerRepo {
 }
