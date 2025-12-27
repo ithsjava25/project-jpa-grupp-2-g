a42e5b770d9eccd36e4fc3cbe88a9adb2bce85bb
Kathrin Trinh <trinhkathrin@hotmail.com>
2025-12-27 12:34:57 +0100

    Update BookingInter.java

--- src/main/java/org/example/interfaces/BookingInter.java
+++ src/main/java/org/example/interfaces/BookingInter.java
@@ -1,4 +1,27 @@
-package org.example.interfaces;
+package org.example.factories;
 
-public class BookingInter {
+import org.example.entities.Booking;
+import org.example.entities.DiningTable;
+import org.example.entities.Customer;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public class BookingFactory {
+
+    public static Booking createBooking(
+            LocalDate date,
+            LocalTime startTime,
+            LocalTime endTime,
+            DiningTable table,
+            List<Customer> customers) {
+
+        Booking booking = new Booking();
+        booking.setDate(date);
+        booking.setStartTime(startTime);
+        booking.setEndTime(endTime);
+        booking.setTable(table);
+        booking.setCustomers(customers);
+        return booking;
+    }
 }
