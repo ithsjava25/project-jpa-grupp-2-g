8f1500f8af8676baeaf09243527c39c88ca1590e
Kathrin Trinh <trinhkathrin@hotmail.com>
2025-12-27 18:08:10 +0100

    RepoInterface

--- src/main/java/org/example/repositories/OpeningHoursRepo.java
+++ src/main/java/org/example/repositories/OpeningHoursRepo.java
@@ -1,17 +1,4 @@
 package org.example.repositories;
 
-import org.example.entities.OpeningHours;
-import java.util.List;
-
-public interface OpeningHoursRepo {
-
-    void save(OpeningHours openingHours);
-
-    OpeningHours findById(Long id);
-
-    List<OpeningHours> findAll();
-
-    void update(OpeningHours openingHours);
-
-    void delete(OpeningHours openingHours);
+public class OpeningHoursRepo {
 }
