4589de2fe5e3cf295e9813445ff78f03f26aa298
Kathrin Trinh <trinhkathrin@hotmail.com>
2025-12-27 18:04:25 +0100

    Update RestaurangInter.java

--- src/main/java/org/example/interfaces/RestaurangInter.java
+++ src/main/java/org/example/interfaces/RestaurangInter.java
@@ -1,16 +1,1 @@
-package org.example.factories;
 
-import org.example.entities.Restaurant;
-import java.util.ArrayList;
-
-public class RestaurantFactory {
-
-    public static Restaurant createRestaurant(String name, String genre, double rating) {
-        Restaurant restaurant = new Restaurant();
-        restaurant.setName(name);
-        restaurant.setGenre(genre);
-        restaurant.setRating(rating);
-        restaurant.setTables(new ArrayList<>()); // tom lista för bord
-        return restaurant;
-    }
-}
