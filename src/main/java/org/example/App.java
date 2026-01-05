bb1c10bac3214f2f193e3a93614b4ec70f6e89dd
EraiicPhu <eraiicphu@gmail.com>
2026-01-05 21:58:07 +0100

    added methods for Repositories + BookingFactory and initial BookingService

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,11 +2,54 @@ package org.example;
 
 import jakarta.persistence.*;
 import org.example.entities.*;
+import org.example.repositories.CustomerRepo;
+import org.example.repositories.RestaurantRepo;
 import org.hibernate.jpa.HibernatePersistenceConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
-        ConnectionProvider.initialize();
+
+        final PersistenceConfiguration cfg = new HibernatePersistenceConfiguration("enf")
+            .jdbcUrl("jdbc:mysql://localhost:3306/app_db")
+            .jdbcUsername("grupp2")
+            .jdbcPassword(System.getenv("PASSWORD"))
+            .property("hibernate.hbm2ddl.auto", "update")
+            .property("hibernate.show_sql", "true")
+            .property("hibernate.format_sql", "true")
+            .property("hibernate.highlight_sql", "true")
+            .managedClasses(Booking.class, Customer.class, DiningTable.class, OpeningHours.class, Restaurant.class);
+
+        try (EntityManagerFactory emf = cfg.createEntityManagerFactory()) {
+            emf.runInTransaction(em -> {
+
+                LocalDateTime now = LocalDateTime.now();
+                LocalDateTime nowPlusTwoHours = LocalDateTime.now().plusHours(2);
+
+                DataLoader loader = new DataLoader();
+                loader.runDataLoader(em);
+
+
+                final RestaurantRepo restaurantRepo = new RestaurantRepo(em);
+                final CustomerRepo customerRepo = new CustomerRepo(em);
+                final BookingFactory bookingFactory = new BookingFactory();
+                Restaurant restaurants = restaurantRepo.fetchRestaurantByName("TOSO");
+
+                Customer customer = customerRepo.fetchCustomerByFirstNameAndLastName("Test", "Testsson");
+
+                Booking b = bookingFactory.createBooking(restaurants, customer, 1l, now, nowPlusTwoHours);
+                em.persist(b);
+                em.flush();
+                System.out.println("BOOKING with ID CREATED: " + b.getId());
+
+
+                //Todo: create entities
+            });
+        }
+
+
     }
-}
+    }
--- src/main/java/org/example/repositories/RestaurantRepo.java
+++ src/main/java/org/example/repositories/RestaurantRepo.java
@@ -1,5 +1,23 @@
 package org.example.repositories;
 
-public class RestaurantRepo {
+import jakarta.persistence.EntityManager;
+import org.example.entities.Restaurant;
 
+import java.util.List;
+
+public class RestaurantRepo {
+    EntityManager em;
+
+    public RestaurantRepo(EntityManager em) {
+        this.em = em;
+    }
+
+    public Restaurant fetchRestaurantByName(String name) {
+        return em.createQuery("""
+            SELECT r FROM Restaurant r
+            WHERE r.name = :name
+            """, Restaurant.class)
+            .setParameter("name", name)
+            .getSingleResultOrNull();
+    }
 }
