420735f0b2153b9463b4ffcc42ba1a3feca145dc
EraiicPhu <eraiicphu@gmail.com>
2026-01-06 19:02:55 +0100

    implemented CustomerRepo

--- src/main/java/org/example/repositories/CustomerRepo.java
+++ src/main/java/org/example/repositories/CustomerRepo.java
@@ -1,9 +1,30 @@
 package org.example.repositories;
 
+import jakarta.persistence.EntityManager;
+import org.example.ConnectionProvider;
 import org.example.entities.Customer;
 
 public class CustomerRepo extends BaseRepo<Customer> {
     public CustomerRepo(){
         super(Customer.class);
+    }
+    EntityManager em;
+
+    public CustomerRepo(EntityManager em) {
+        this.em = em;
+    }
+    public Customer fetchCustomerByFirstNameAndLastName(String firstName, String lastName) {
+        try (EntityManager em =
+                 ConnectionProvider.getEMF().createEntityManager()) {
+            return em.createQuery("""
+                    SELECT c FROM Customer c
+        WHERE c.firstName = :firstName
+        AND  c.lastName = :lastName
+        """, Customer.class)
+            .setParameter("firstName", firstName)
+            .setParameter("lastName", lastName)
+            .getSingleResultOrNull();
+        }
+
     }
 }
