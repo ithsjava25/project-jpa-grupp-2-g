3a61f48ef2fd495f24be5cc4ca5a19a9d6878583
Anna Ziafar <annaaziafar@gmail.com>
2026-01-07 19:12:49 +0100

    Create MVC, hello-view.fxml and configure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module hellofx {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.net.http;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires java.xml;
+
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.sql;
+
+    opens frontend to javafx.fxml;
+    exports frontend;
+}
