edcecaa7018c92dcc794b2630908c637db9a3fe9
Anna Ziafar <annaaziafar@gmail.com>
2026-01-07 17:18:19 +0100

    Create customer service and add validation

---
+++ src/main/java/org/example/services/CustomerService.java
@@ -0,0 +1,26 @@
+package org.example.services;
+
+public class CustomerService {
+
+    public boolean validateName(String name){
+        if(name == null || name.isBlank())
+            return false;
+
+        return name.matches("^[a-zA-ZåäöÅÄÖ]+$");
+    }
+
+    public boolean validatePhoneNumber(String number){
+        if(number.length() != 10)
+            return false;
+
+        return number.matches("^0[0-9]{9}$");
+    }
+
+    public boolean validateEmail(String email){
+        if(email == null || email.isBlank())
+            return false;
+
+        return email.matches("^[a-zA-ZåäöÅÄÖ]{1,20}@[a-zåäöÅÄÖ]{1,10}(\\.se|\\.com)$");
+    }
+
+}
