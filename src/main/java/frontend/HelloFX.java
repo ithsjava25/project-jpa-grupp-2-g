c277013484cd32e1aeee091d419f859263c94f94
Anna Ziafar <annaaziafar@gmail.com>
2026-01-07 19:18:37 +0100

    Create MVC, hello-view.fxml and configure

--- src/main/java/frontend/HelloFX.java
+++ src/main/java/frontend/HelloFX.java
@@ -1,5 +1,13 @@
 package frontend;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HelloFX extends Application {
 
     @Override
