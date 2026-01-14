4efb74d4af24536d7b0807ee03615d82a5d12984
Kathrin Trinh <trinhkathrin@hotmail.com>
2026-01-14 19:22:36 +0100

    Delete src/main/java/backend/repositories directory

--- src/main/java/backend/repositories/OpeningHoursRepo.java
+++
@@ -1,9 +0,0 @@
-package backend.repositories;
-
-import backend.entities.OpeningHours;
-
-public class OpeningHoursRepo extends BaseRepo<OpeningHours> {
-    public OpeningHoursRepo(){
-        super(OpeningHours.class);
-    }
-}
