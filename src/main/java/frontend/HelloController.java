60f63454e8235253a832795eb2d7438aaa8e6c36
Kathify <Trinhkathrin@hotmail.com>
2026-01-14 08:52:34 +0100

    Logo och sökruta

--- src/main/java/frontend/HelloController.java
+++ src/main/java/frontend/HelloController.java
@@ -1,39 +1,67 @@
 package frontend;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 public class HelloController {
+
     private final HelloModel model = new HelloModel();
-    private CategoryManager categoryManager;
 
     @FXML
-    private Label appName;
+    private BorderPane rootPane;
+
     @FXML
-    private TextField searchRestaurantField;
+    private ImageView logoImage;
+
+    @FXML
+    private StackPane headerContainer;
+
     @FXML
     private BorderPane categoryContainer;
+
+    @FXML
+    private TextField searchRestaurantField;
+
     @FXML
     private GridPane grid;
 
+    private CategoryManager categoryManager;
+    private HeaderManager headerManager;
+
     @FXML
     private void initialize() {
-        appName.setText(model.getAppName());
+        // header
+        headerManager = new HeaderManager(this);
+        headerManager.createHeader();
+
+        // kategorigrid
         categoryManager = new CategoryManager(this);
         categoryManager.createCategoryGrid();
     }
 
     @FXML
-    public void handleRestaurantSearch() {}
+    private void handleRestaurantSearch() {
+        // filtrera restauranger
+    }
+
+    public BorderPane getRootPane() {
+        return rootPane;
+    }
+
+    public BorderPane getCategoryContainer() {
+        return categoryContainer;
+    }
 
     public GridPane getGrid() {
         return grid;
     }
 
-    public BorderPane getCategoryContainer() {
-        return categoryContainer;
+    // getter för headerContainer
+    public StackPane getHeaderContainer() {
+        return headerContainer;
     }
 }
