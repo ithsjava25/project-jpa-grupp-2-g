def31a9146889be7f6c6190f800a9d4f3370190f
Kathrin Trinh <trinhkathrin@hotmail.com>
2026-01-14 09:35:33 +0100

    Delete src/main/java/frontend/HelloFX.java

--- src/main/java/frontend/CategoryFX.java
+++ src/main/java/frontend/CategoryFX.java
@@ -8,14 +8,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CategoryFX extends Application {
+public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CategoryFX.class.getResource("category-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloFX.class.getResource("category-view.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 640, 480);
-        scene.getStylesheets().add(getClass().getResource("category.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         stage.setTitle("Restaurangbokningar");
         stage.setScene(scene);
         stage.show();
