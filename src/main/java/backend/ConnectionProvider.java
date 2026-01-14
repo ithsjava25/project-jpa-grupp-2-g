5c63e23452ab1a25ac2ec01e74be6590c407383e
EraiicPhu <eraiicphu@gmail.com>
2026-01-14 01:39:44 +0100

    Added Testcontainer with testDB. implemented function so that we can connect to correct DB depending if test or not. added necessary dependencies

--- src/main/java/backend/ConnectionProvider.java
+++ src/main/java/backend/ConnectionProvider.java
@@ -18,17 +18,34 @@ public class ConnectionProvider {
 
     static{
         try{
-            cfg = new HibernatePersistenceConfiguration("persistence")
-                .jdbcUrl("jdbc:mysql://localhost:3306/app_db?createDatabaseIfNotExist=true")
-                .jdbcUsername("grupp2")
-                .jdbcPassword(env.get("PASSWORD"))
-                .property("hibernate.connection.provider_class", "org.hibernate.hikaricp.internal.HikariCPConnectionProvider")
-                .property("hibernate.hbm2ddl.auto", "update")
-                .property("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
-                .property("hibernate.show_sql", "true")
-                .property("hibernate.format_sql", "true")
-                .property("hibernate.highlight_sql", "true")
-                .managedClasses(Booking.class, Customer.class, DiningTable.class, OpeningHours.class, Restaurant.class);
+            if(env.get("TEST").equals("TRUE")){
+
+                cfg = new HibernatePersistenceConfiguration("persistence")
+                    .jdbcUrl("jdbc:mysql://localhost:3307/testdb")
+                    .jdbcUsername("user")
+                    .jdbcPassword("password")
+                    .property("hibernate.connection.provider_class", "org.hibernate.hikaricp.internal.HikariCPConnectionProvider")
+                    .property("hibernate.hbm2ddl.auto", "update")
+                    .property("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .property("hibernate.show_sql", "true")
+                    .property("hibernate.format_sql", "true")
+                    .property("hibernate.highlight_sql", "true")
+                    .managedClasses(Booking.class, Customer.class, DiningTable.class, OpeningHours.class, Restaurant.class);
+            }
+            else{
+                cfg = new HibernatePersistenceConfiguration("persistence")
+                    .jdbcUrl("jdbc:mysql://localhost:3306/app_db?createDatabaseIfNotExist=true")
+                    .jdbcUsername("grupp2")
+                    .jdbcPassword(env.get("PASSWORD"))
+                    .property("hibernate.connection.provider_class", "org.hibernate.hikaricp.internal.HikariCPConnectionProvider")
+                    .property("hibernate.hbm2ddl.auto", "update")
+                    .property("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .property("hibernate.show_sql", "true")
+                    .property("hibernate.format_sql", "true")
+                    .property("hibernate.highlight_sql", "true")
+                    .managedClasses(Booking.class, Customer.class, DiningTable.class, OpeningHours.class, Restaurant.class);
+
+            }
 
                 EMF = cfg.createEntityManagerFactory();
         } catch (Exception e) {
